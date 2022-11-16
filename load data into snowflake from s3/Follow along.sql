49c582dc39310614d3920478e9daf8f0025016b7
Tim Ngwena <ngwena@gmail.com>
2022-11-16 21:21:20 +0000

    Update Follow along.sql

--- load data into snowflake from s3/Follow along.sql
+++ load data into snowflake from s3/Follow along.sql
@@ -1,11 +1,11 @@
-//Create some structure DB, Schema, external stage s3://tableautim-snow/yt-datasets/
+//Create some structure DB, Schema, external stage [your s3 uri]
 
 create or replace database staging ;
 
 create or replace schema external_staging;
 
 create or replace stage aws_stage
-url='s3://tableautim-snow/yt-datasets/';
+url='[your s3 uri]';
 
 desc stage aws_stage;
 
@@ -22,7 +22,7 @@ select t.$1, t.$2,t.$3, t.$4,t.$5  from @aws_stage
 t;
 
 //Create a table to load data into 
-create table customer_tablev3 (
+create table customer_table (
     ID INT,
     first_name varchar,
     last_name varchar,
@@ -31,17 +31,26 @@ create table customer_tablev3 (
 );
 
 
-//Load into the table 
-
+//Load into the table Firest example
+//file formarts are schema specific
 create or replace file format csv_pipe type= 'csv' field_delimiter='|' skip_header=1;
 
-copy into customer_tablev2 from (
+copy into customer_table from (
                                 select t.$1, t.$2,t.$3, t.$4,t.$5  from @staging.external_staging.aws_stage
                                 (file_format => 'csv_pipe')
                                 t);
 
 
-copy into customer_tablev3 
+//Load into the table Firest example
+create table customer_tablev2 (
+    ID INT,
+    first_name varchar,
+    last_name varchar,
+    favourite_app varchar,
+    favourite_color varchar
+);
+
+copy into customer_tablev2
 from @staging.external_staging.aws_stage
 file_format = (type= 'csv' field_delimiter='|' skip_header=1);
 
